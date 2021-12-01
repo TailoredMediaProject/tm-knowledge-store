a8942dcfc7ceb2b2d572a67e6665c6ac865121e2
Jan Cortiel <jan.cortiel@redlink.at>
2021-12-01 12:32:56 +0100

    TM-96: Dummy Test fix

--- fe/src/components/ListView.vue
+++ fe/src/components/ListView.vue
@@ -2,9 +2,7 @@
   <div class="flex flex-col">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-        <div
-          class="shadow overflow-hidden border-gray-200 sm:rounded-lg"
-        >
+        <div class="shadow overflow-hidden border-gray-200 sm:rounded-lg">
           <table class="min-w-full divide-y divide-gray-200">
             <thead class="bg-gray-50">
               <tr>
@@ -105,9 +103,7 @@
                     font-medium
                   "
                 >
-                  <a href="#" class="text-tmBlue hover:text-indigo-900"
-                    >Edit</a
-                  >
+                  <a href="#" class="text-tmBlue hover:text-indigo-900">Edit</a>
                 </td>
               </tr>
             </tbody>
