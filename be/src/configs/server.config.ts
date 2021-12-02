07294bd8c0f79c31fcfae5b6c3af4e2439472109
Manuel T. Schrempf <manuel.schrempf@redbull.com>
2021-12-02 09:25:51 +0100

    TM-94: Gen working TS models in BE

--- be/src/configs/server.config.ts
+++ be/src/configs/server.config.ts
@@ -2,5 +2,5 @@ import ServerConfigModel from '../models/server-config.model';
 
 export default class ServerConfig implements ServerConfigModel {
   host = process.env.HOST || '0.0.0.0';
-  port = parseInt(process.env.PORT || '0', 10);
+  port = parseInt(process.env.PORT || '8080', 10);
 }
