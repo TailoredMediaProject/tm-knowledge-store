8be4306c2e4843d3425b63920d02b3ed5c730b2b
Manuel T. Schrempf <8605616+ManuelTS@users.noreply.github.com>
2021-12-15 13:54:04 +0100

    TM-86: List vocabs (#12)
    * TM-86: List + read vocab
    
    * Merge pull request #11 from TailoredMediaProject/TM-68-implement_entity_management
    
    TM-68: Created entity-service
    
    * Use rows according to spec
    
    * Fix linting error in health check
    
    the disable lint comment does not function on the
    github action
    
    * Fix linting errors from FE
    
    * TM-87: Use KnowledgeErrors
    
    Co-authored-by: Manuel T. Schrempf <manuel.schrempf@redbull.com>

--- be/src/services/vocabulary.service.ts
+++ be/src/services/vocabulary.service.ts
@@ -1,6 +1,8 @@
-import {Collection, ObjectId} from 'mongodb';
+import {Collection, Filter, FindOptions, ObjectId} from 'mongodb';
 import {Vocabulary} from '../models/dbo.models';
 import {instance, PersistenceService} from './persistence.service';
+import ListQueryModel from '../models/query-list.model';
+import {ListingResult} from '../models/listing-result.model';
 
 export class VocabularyService {
     private readonly persistence: PersistenceService = instance
@@ -24,8 +26,101 @@ export class VocabularyService {
         return this.collection.findOne({_id: new ObjectId(id)}).then(x => <Vocabulary>x);
     }
 
-    public readVocab(): void {
-        // TODO
+    // eslint-disable-rows-line @typescript-eslint/explicit-module-boundary-types
+    public async listVocab(query: ListQueryModel, id?: string | ObjectId): Promise<ListingResult<Vocabulary>> {
+        const { options, filter } = this.transformToMongoDBFilterOption(query, id);
+        // @ts-ignore
+        const dbos: Vocabulary[] = await this.collection.find(filter, options).toArray() as Vocabulary[];
+        return {
+            offset: query.offset,
+            rows: dbos.length,
+            totalItems: 0, // TODO
+            items: dbos
+        };
+    }
+
+    private escapeRegExp(string: string): string {
+        return string.replace(/[.*+?^${}()|[\]\\]/g, '');
+    }
+
+    private transformToMongoDBFilterOption(query?: ListQueryModel, id?: string | ObjectId):
+      { options: FindOptions, filter: Filter<Vocabulary> } {
+        const options: FindOptions = {};
+        const filter: Filter<Vocabulary> = {};
+
+        if (!!id) {
+            // @ts-ignore
+            filter._id = new ObjectId(id);
+        }
+
+        if (!!query) {
+            if (!!query?.text) {
+                filter.$or = [
+                    {
+                        label: {
+                            $regex: this.escapeRegExp(query.text),
+                            $options: 'gi'
+                        }
+                    },
+                    {
+                        description: {
+                            $regex: this.escapeRegExp(query.text),
+                            $options: 'gi'
+                        }
+                    }
+                ];
+            }
+
+            if (!!query?.createdSince) {
+                // @ts-ignore
+                filter.created = {
+                    // eslint-disable-rows-line @typescript-eslint/no-unsafe-argument
+                    $gte: new Date(query?.createdSince)
+                    // https://www.mongodb.com/community/forums/t/finding-data-between-two-dates-by-using-a-query-in-mongodb-charts/102506/2
+                };
+            }
+
+            if (!!query?.modifiedSince) {
+                // @ts-ignore
+                filter.lastModified = {
+                    // eslint-disable-rows-line @typescript-eslint/no-unsafe-argument
+                    $gte: new Date(query.modifiedSince)
+                };
+            }
+
+            if (!!query?.sort) {
+                // @ts-ignore
+                options.sort = this.mapToMongoSort(query?.sort);
+            }
+
+            if (!!query?.offset) {
+                options.skip = Number(query?.offset); // Without the cast, it won't work!
+            }
+
+            if (!!query?.rows) {
+                options.limit = Number(query?.rows); // Without the cast, it won't work!
+            }
+        }
+
+        return {
+            options,
+            filter
+        };
+    }
+
+    private mapToMongoSort(sort: string): unknown {
+        if (!!sort && sort.includes(' ')) {
+            if (sort.toLowerCase().includes('created')) {
+                return {
+                    created: sort.toLowerCase().includes('asc') ? 1 : -1
+                };
+            } else {
+                return {
+                    lastModified: sort.toLowerCase().includes('asc') ? 1 : -1
+                };
+            }
+        }
+        return {};
     }
 }
 
