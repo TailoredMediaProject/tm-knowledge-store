fb70924853bda26e25df11108ed063fd130f774b
Daniil <barkovdc@gmail.com>
2021-12-15 13:49:25 +0100

    Added `Location-Header` and set `statusCode` to 201 in `POST`

--- be/src/controllers/rest.router.ts
+++ be/src/controllers/rest.router.ts
@@ -25,72 +25,77 @@ const vocabDbo2Dto = (dbo: Vocabulary): VocabularyDTO => ({
 });
 
 router.get('/vocab', (req: Request, res: Response, next: NextFunction) => {
-  next(new KnowledgeError(501, 'Not Implemented', 'GET /vocab is not implemented'));
+    next(new KnowledgeError(501, 'Not Implemented', 'GET /vocab is not implemented'));
 });
 
 router.post('/vocab', (req: Request, res: Response, next: NextFunction) => {
-  const body = <VocabularyDTO>req.body
-  const newVocab = vocabDto2Dbo(body)
+    const body = <VocabularyDTO>req.body
+    const newVocab = vocabDto2Dbo(body)
+    let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
 
-  vocabularyService.createVocab(newVocab)
-      .then(v => vocabDbo2Dto(v))
-      .then(v => res.json(v))
-      .catch((e: unknown) => next(new KnowledgeError(500, 'Internal Server Error', e.toString())));
+    vocabularyService.createVocab(newVocab)
+        .then(v => vocabDbo2Dto(v))
+        .then(v => {
+            fullUrl += v.id;
+            res.setHeader('Location', fullUrl);
+            res.status(201).json(v)
+        })
+        .catch((e: unknown) => next(new KnowledgeError(500, 'Internal Server Error', e.toString())));
 });
 
 router.put('/vocab/:id', (req: Request, res: Response, next: NextFunction) => {
-  next(new KnowledgeError(501, 'Not Implemented', 'PUT /vocab/:id is not implemented'));
+    next(new KnowledgeError(501, 'Not Implemented', 'PUT /vocab/:id is not implemented'));
 });
 
 router.get('/vocab/:id', (req: Request, res: Response, next: NextFunction) => {
-  vocabularyService.getVocabular(req.params.id)
-      .then(v => vocabDbo2Dto(v))
-      .then(v => res.json(v))
-      .catch((e: unknown) => next(new KnowledgeError(500, 'Internal Server Error', e.toString())));
+    vocabularyService.getVocabular(req.params.id)
+        .then(v => vocabDbo2Dto(v))
+        .then(v => res.json(v))
+        .catch((e: unknown) => next(new KnowledgeError(500, 'Internal Server Error', e.toString())));
 });
 
 router.delete('/vocab/:id', (req: Request, res: Response, next: NextFunction) => {
-  next(new KnowledgeError(501, 'Not Implemented', 'DELETE /vocab/:id is not implemented'));
+    next(new KnowledgeError(501, 'Not Implemented', 'DELETE /vocab/:id is not implemented'));
 });
 
 router.get('/vocab/:id/entities', (req: Request, res: Response, next: NextFunction) => {
-  try {
-    void entityServiceInstance.getEntity(undefined, undefined);
-  } catch(e) {
-    next(e);
-  }
+    try {
+        void entityServiceInstance.getEntity(undefined, undefined);
+    } catch (e) {
+        next(e);
+    }
 });
 
 router.post('/vocab/:id/entities', (req: Request, res: Response, next: NextFunction) => {
-  try {
-    void entityServiceInstance.getEntity(undefined, undefined);
-  } catch(e) {
-    next(e);
-  }
+    try {
+        void entityServiceInstance.getEntity(undefined, undefined);
+    } catch (e) {
+        next(e);
+    }
 });
 
 router.get('/vocab/:id/entities/:id', (req: Request, res: Response, next: NextFunction) => {
-  try {
-    void entityServiceInstance.getEntity(undefined, undefined);
-  } catch(e) {
-    next(e);
-  }
+    try {
+        void entityServiceInstance.getEntity(undefined, undefined);
+    } catch (e) {
+        next(e);
+    }
 });
 
 router.put('/vocab/:id/entities/:id', (req: Request, res: Response, next: NextFunction) => {
-  try {
-    void entityServiceInstance.getEntity(undefined, undefined);
-  } catch(e) {
-    next(e);
-  }
+    try {
+        void entityServiceInstance.getEntity(undefined, undefined);
+    } catch (e) {
+        next(e);
+    }
 });
 
 router.delete('/vocab/:id/entities/:id', (req: Request, res: Response, next: NextFunction) => {
-  try {
-    void entityServiceInstance.getEntity(undefined, undefined);
-  } catch(e) {
-    next(e);
-  }
+    try {
+        void entityServiceInstance.getEntity(undefined, undefined);
+    } catch (e) {
+        next(e);
+    }
 });
 
 export default router;
