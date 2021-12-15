3bc7aaf9f775defd7e7207fb54229831386fef5b
Daniil Barkov <barkovdc@gmail.com>
2021-12-15 14:11:59 +0100

    Added `Location-Header` and set `statusCode` to 201 in `POST` (#21)
    * Added `Location-Header` and set `statusCode` to 201 in `POST`
    
    * TM-87: Use KnowledgeErrors
    
    Co-authored-by: Manuel T. Schrempf <manuel.schrempf@redbull.com>

--- be/src/controllers/rest.router.ts
+++ be/src/controllers/rest.router.ts
@@ -10,20 +10,20 @@ import ListQueryModel from '../models/query-list.model';
 const router: Router = Router();
 
 const vocabDto2Dbo = (dto: VocabularyDTO): Vocabulary => ({
-    _id: undefined,
-    created: undefined,
-    description: dto.description,
-    label: dto.label,
-    lastModified: undefined
+  _id: undefined,
+  created: undefined,
+  description: dto.description,
+  label: dto.label,
+  lastModified: undefined
 });
 
 const vocabDbo2Dto = (dbo: Vocabulary): VocabularyDTO => ({
-    id: dbo._id.toHexString(),
-    label: dbo.label,
-    description: dbo.description,
-    created: dbo.created.toISOString(),
-    lastModified: dbo.lastModified.toISOString(),
-    entityCount: -1
+  id: dbo._id.toHexString(),
+  label: dbo.label,
+  description: dbo.description,
+  created: dbo.created.toISOString(),
+  lastModified: dbo.lastModified.toISOString(),
+  entityCount: -1
 });
 
 router.get('/vocab', (req: Request, res: Response, next: NextFunction) => {
@@ -33,24 +33,30 @@ router.get('/vocab', (req: Request, res: Response, next: NextFunction) => {
     const queryListModel: ListQueryModel = {
       ...req?.query,
       modifiedSince: !!req?.query?.modifiedSince ? new Date(`${req?.query.modifiedSince}`) : undefined,
-      createdSince: !!req?.query?.createdSince ? new Date(`${req?.query.createdSince}`) : undefined,
+      createdSince: !!req?.query?.createdSince ? new Date(`${req?.query.createdSince}`) : undefined
     };
 
     vocabularyService.listVocab(queryListModel, req.params.id)
-      .then((r: ListingResult<Vocabulary>) => ({...r, items: r.items.map((v: Vocabulary) => vocabDbo2Dto(v))}))
+      .then((r: ListingResult<Vocabulary>) => ({ ...r, items: r.items.map((v: Vocabulary) => vocabDbo2Dto(v)) }))
       .then((r: ListingResult<VocabularyDTO>) => res.json(r))
       .catch(next);
   }
 });
 
 router.post('/vocab', (req: Request, res: Response, next: NextFunction) => {
-  const body = <VocabularyDTO>req.body
-  const newVocab = vocabDto2Dbo(body)
+  const body = <VocabularyDTO> req.body;
+  const newVocab = vocabDto2Dbo(body);
+
 
   vocabularyService.createVocab(newVocab)
-      .then(v => vocabDbo2Dto(v))
-      .then(v => res.json(v))
-      .catch((e: unknown) => next(new KnowledgeError(500, 'Internal Server Error', e.toString())));
+    .then(v => vocabDbo2Dto(v))
+    .then(v => {
+      const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}/${v.id}`;
+
+      res.setHeader('Location', fullUrl);
+      res.status(201).json(v);
+    })
+    .catch(next);
 });
 
 router.put('/vocab/:id', (req: Request, res: Response, next: NextFunction) => {
@@ -59,9 +65,9 @@ router.put('/vocab/:id', (req: Request, res: Response, next: NextFunction) => {
 
 router.get('/vocab/:id', (req: Request, res: Response, next: NextFunction) => {
   vocabularyService.getVocabular(req.params.id)
-      .then(v => vocabDbo2Dto(v))
-      .then(v => res.json(v))
-      .catch((e: unknown) => next(new KnowledgeError(500, 'Internal Server Error', e.toString())));
+    .then(v => vocabDbo2Dto(v))
+    .then(v => res.json(v))
+    .catch(next);
 });
 
 router.delete('/vocab/:id', (req: Request, res: Response, next: NextFunction) => {
@@ -71,7 +77,7 @@ router.delete('/vocab/:id', (req: Request, res: Response, next: NextFunction) =>
 router.get('/vocab/:id/entities', (req: Request, res: Response, next: NextFunction) => {
   try {
     void entityServiceInstance.getEntity(undefined, undefined);
-  } catch(e) {
+  } catch (e) {
     next(e);
   }
 });
@@ -79,7 +85,7 @@ router.get('/vocab/:id/entities', (req: Request, res: Response, next: NextFuncti
 router.post('/vocab/:id/entities', (req: Request, res: Response, next: NextFunction) => {
   try {
     void entityServiceInstance.getEntity(undefined, undefined);
-  } catch(e) {
+  } catch (e) {
     next(e);
   }
 });
@@ -87,7 +93,7 @@ router.post('/vocab/:id/entities', (req: Request, res: Response, next: NextFunct
 router.get('/vocab/:id/entities/:id', (req: Request, res: Response, next: NextFunction) => {
   try {
     void entityServiceInstance.getEntity(undefined, undefined);
-  } catch(e) {
+  } catch (e) {
     next(e);
   }
 });
@@ -95,7 +101,7 @@ router.get('/vocab/:id/entities/:id', (req: Request, res: Response, next: NextFu
 router.put('/vocab/:id/entities/:id', (req: Request, res: Response, next: NextFunction) => {
   try {
     void entityServiceInstance.getEntity(undefined, undefined);
-  } catch(e) {
+  } catch (e) {
     next(e);
   }
 });
@@ -103,7 +109,7 @@ router.put('/vocab/:id/entities/:id', (req: Request, res: Response, next: NextFu
 router.delete('/vocab/:id/entities/:id', (req: Request, res: Response, next: NextFunction) => {
   try {
     void entityServiceInstance.getEntity(undefined, undefined);
-  } catch(e) {
+  } catch (e) {
     next(e);
   }
 });
