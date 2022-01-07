19456c9807426506bb24598f453be9b3518b4ebf
Manuel T. Schrempf <manuel.schrempf@redbull.com>
2022-01-07 11:23:12 +0100

    TM-88: Manually resolve search URL
    by using entities service

--- be/src/services/knowledge-resolve.service.ts
+++ be/src/services/knowledge-resolve.service.ts
@@ -1,10 +1,12 @@
 import {ResolveService} from '../models/resolve-service.interface';
-import {vocabularyService} from './vocabulary.service';
 import {KnowledgeError} from '../models/knowledge-error.model';
 import {UtilService} from './util.service';
+import {entityServiceInstance} from './entity.service';
 
 export default class KnowledgeResolveService implements ResolveService {
   private readonly host = 'data.tmedia.redlink.io';
+  private readonly subPathVocabs = '/vocab';
+  private readonly subPathEntities = '/entities';
   private processUrl: URL;
 
   accept(url: URL): boolean {
@@ -22,13 +24,35 @@ export default class KnowledgeResolveService implements ResolveService {
 
   resolve(): Promise<unknown> {
     if(!!this.processUrl) {
-      return vocabularyService
-        .getVocabular(this.processUrl.searchParams.get('id'))
+      const vocabId = this.getSubPath(true);
+      const entityId = this.getSubPath(false);
+
+      return entityServiceInstance
+        .getEntity(vocabId, entityId)
         .then((v) => UtilService.vocabDbo2Dto(v))
         .catch((e: unknown) => {
           throw new KnowledgeError(501, 'Internal Server Error', e.toString());
         });
     }
     throw new KnowledgeError(501, 'Internal Server Error', 'URL to resolve is falsy');
+  }
+
+  private getSubPath(vocab: boolean): string {
+    const path = this.processUrl.pathname;
+    const subPath = vocab ? this.subPathVocabs : this.subPathEntities;
+
+    if(!this.processUrl.pathname.includes(subPath)) {
+      throw new KnowledgeError(400,
+        'Bad Request',
+        `The URL to resolve '${this.processUrl}' does not contain the correct '${subPath}' path`
+      );
+    }
+
+    const from = vocab ?
+      path.indexOf(this.subPathVocabs) + this.subPathVocabs.length + 2 :
+      path.indexOf(this.subPathEntities) + this.subPathEntities.length + 1;
+    const to = vocab ? path.indexOf(this.subPathEntities) : path.length;
+
+    return path.substring(from, to)
   }
 }
