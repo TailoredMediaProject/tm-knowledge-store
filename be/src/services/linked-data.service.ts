62bd73fa112d2f1438bea2047f4bfbab382d5143
Manuel T. Schrempf <manuel.schrempf@redbull.com>
2022-01-19 14:10:57 +0100

    TM-92: Create .ttl files with rdflib.js

--- be/src/services/linked-data.service.ts
+++ be/src/services/linked-data.service.ts
@@ -1,8 +1,9 @@
 import {Entity} from '../models/dbo.models';
-import {DataFactory, Literal, Writer} from 'n3';
-import {MIME_TYPE_TURTLE, PROPERTY_MAPPING_CONFIG} from '../models/constants';
+import {HOST, MIME_TYPE_TURTLE, PROPERTY_MAPPING_CONFIG} from '../models/constants';
 import {NextFunction} from 'express';
 import {UtilService} from './util.service';
+import Store from 'rdflib/lib/store';
+import $rdf = require('rdflib');
 
 class LinkedDataService {
   public readonly entityDbo2LinkedData = (e: Entity, mimeType: string, next: NextFunction): string => {
@@ -14,39 +15,22 @@ class LinkedDataService {
   };
 
   private readonly entityDbo2Turtle = (e: Entity): string => {
-    const prefixUrl = PROPERTY_MAPPING_CONFIG.entity.prefixUrl;
-    const writer = new Writer({
-      format: MIME_TYPE_TURTLE,
-      prefixes: {[PROPERTY_MAPPING_CONFIG.entity.prefix]: prefixUrl}
-    });
-
-    writer.addQuad(
-      DataFactory.namedNode(`${e._id.toHexString()}`),
-      DataFactory.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
-      DataFactory.namedNode(`${e.type}`)
-    );
+    const store: Store = $rdf.graph();
+    const base = $rdf.Namespace(`https://${HOST}/kb/`);
+    const prefix = $rdf.Namespace(PROPERTY_MAPPING_CONFIG.entity.prefixUrl);
 
     const mapping = PROPERTY_MAPPING_CONFIG.entity.properties;
     Object.keys(mapping).forEach((key: string) => {
-      writer.addQuad(DataFactory.quad(
-        DataFactory.namedNode(`${e._id.toHexString()}`),
-        // @ts-ignore
-        DataFactory.namedNode(`${prefixUrl}${mapping[key]}`),
-        // @ts-ignore
-        this.typeTurtleValue(e[key])
-      ));
+      // @ts-ignore
+      store.add(base(`${e._id.toHexString()}`), prefix(`${mapping[key]}`), `${e[key]}`);
     });
 
-    let rdf: string;
-    writer.end((error, result) => rdf = result);
-    return rdf;
+    let turtle: string;
+    // @ts-ignore
+    $rdf.serialize(undefined, store, `https://${HOST}/kb/`, MIME_TYPE_TURTLE, (err, data) => turtle = data);
+    console.log(turtle);
+    return turtle;
   };
-
-  private readonly typeTurtleValue = (value: unknown): Literal => value instanceof Date ?
-    DataFactory.literal(
-      value.toISOString(),
-      DataFactory.namedNode('xsd:Date')
-    ) : DataFactory.literal(`${value}`);
 }
 
 export const linkedDataServiceInstance = new LinkedDataService();
