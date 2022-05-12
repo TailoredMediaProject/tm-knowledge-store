2c58814f0d05ba949d552742e81319aeda258dcc
Manuel T. Schrempf <manuel.schrempf@redbull.com>
2022-05-12 12:25:59 +0200

    TM-151: Regenerate missing AA entities

--- be/src/services/constant.service.ts
+++ be/src/services/constant.service.ts
@@ -44,38 +44,73 @@ class ConstantService {
 
   private readonly initEntities = (vocab: Vocabulary): void => {
     if (this.backupFileNonExistent()) {
-      const entries: AutomaticAnalysisModel[] = csvToJson
-        .fieldDelimiter(';')
-        .getJsonFromCsv(path.join(__dirname, this.AUTOMATIC_ANALYSIS_FILE))
-        .map((rawElement: unknown): AutomaticAnalysisModel => ({
-          // @ts-ignore
-          eId: rawElement['ExternalID(Johenneum)'],
-          // @ts-ignore
-          label: rawElement['Label(DE)'],
-          // @ts-ignore
-          entityType: rawElement.EntityType,
-          // @ts-ignore
-          tagTree: rawElement['Tag-TreeType'],
-          // @ts-ignore
-          canonicalLink: rawElement['Tag-TreeType']
-        }));
-
-      Promise.all(entries.map((aam: AutomaticAnalysisModel) =>
-        entityServiceInstance.createEntity(UtilService.aam2EntityDbo(aam, vocab._id)))).then((savedEntities: Entity[]): void => {
-        // Save backup
-        fs.writeFileSync(path.join(__dirname, this.AUTOMATIC_ANALYSIS_BACKUP), JSON.stringify(savedEntities, null, 2));
-        // Save id / link list
-        fs.writeFileSync(path.join(__dirname, this.AUTOMATIC_ANALYSIS_ID_LIST), JSON.stringify(
-          savedEntities.map((e: Entity) => ({
-            label: e.externalResources[0],
-            canonicalLink: UtilService.createCanonicalLink(e?._id?.toHexString())
-          })), null, 2));
-        console.log(`${this.LOG_TAG} Generated backup and ID list successfully`);
-      })
-        .catch(console.error);
+      this.createEntries(vocab);
     } else {
-      console.log(`${this.LOG_TAG} Backup exists, no new AA entities were created`);
+      this.loadEntries(vocab);
     }
+  };
+  private readonly loadEntries = (vocab: Vocabulary): void => {
+    // @ts-ignore
+    const entities: Entity[] = JSON.parse(fs.readFileSync(path.join(__dirname, this.AUTOMATIC_ANALYSIS_BACKUP)));
+    let created = false;
+
+    Promise.all(
+      entities.map((e: Entity) =>
+        entityServiceInstance.getEntityWithoutVocab(e._id)
+          .then(() => undefined) // Entity exists on DB, do nothing
+          .catch((err) => {
+            console.log(err);
+            return e;
+          }) // Entity not found, so create it
+      )
+    ).then((nonExistingEntities: Entity []) => {
+      nonExistingEntities.filter(e => !!e)
+        .forEach((nE: Entity): void => {
+          nE.vocabulary = vocab._id;
+          entityServiceInstance.createEntity(nE)
+            .then((): void => {created = true;})
+            .catch(console.error);
+        });
+    })
+      .catch(console.error);
+
+    if(created) {
+      console.log(`${this.LOG_TAG} Backup exists, AA entities were restored`);
+    } else {
+      console.log(`${this.LOG_TAG} Backup exists, no AA entities were changed nor restored`);
+    }
+  };
+
+  private readonly createEntries = (vocab: Vocabulary): void => {
+    const entries: AutomaticAnalysisModel[] = csvToJson
+      .fieldDelimiter(';')
+      .getJsonFromCsv(path.join(__dirname, this.AUTOMATIC_ANALYSIS_FILE))
+      .map((rawElement: unknown): AutomaticAnalysisModel => ({
+        // @ts-ignore
+        eId: rawElement['ExternalID(Johenneum)'],
+        // @ts-ignore
+        label: rawElement['Label(DE)'],
+        // @ts-ignore
+        entityType: rawElement.EntityType,
+        // @ts-ignore
+        tagTree: rawElement['Tag-TreeType'],
+        // @ts-ignore
+        canonicalLink: rawElement['Tag-TreeType']
+      }));
+
+    Promise.all(entries.map((aam: AutomaticAnalysisModel) =>
+      entityServiceInstance.createEntity(UtilService.aam2EntityDbo(aam, vocab._id)))).then((savedEntities: Entity[]): void => {
+      // Save backup
+      fs.writeFileSync(path.join(__dirname, this.AUTOMATIC_ANALYSIS_BACKUP), JSON.stringify(savedEntities, null, 2));
+      // Save id / link list
+      fs.writeFileSync(path.join(__dirname, this.AUTOMATIC_ANALYSIS_ID_LIST), JSON.stringify(
+        savedEntities.map((e: Entity) => ({
+          label: e.externalResources[0],
+          canonicalLink: UtilService.createCanonicalLink(e?._id?.toHexString())
+        })), null, 2));
+      console.log(`${this.LOG_TAG} Generated backup and ID list successfully`);
+    })
+      .catch(console.error);
   };
 
   private readonly backupFileNonExistent = (): boolean => {
